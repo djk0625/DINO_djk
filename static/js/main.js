e7104d5cab25f4e3bd87669dcbd5677b6301ace8
djk0625 <djk0625@naver.com>
2022-11-02 03:20:07 +0900

    DINO_FINAL

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,3 @@
-main.js
-
 const currentPage = window.location.href;
 
 const list = ["home", "API", "list", "collection", "mypage"];
@@ -149,3 +147,124 @@ function cancel(){
     $("#textF19").val("");
     $("#textF20").val("");
  }
+
+// 카테고리 수정 취소 버튼 클릭
+ function cancelCategory() {
+    $("#autoInput1").val("");
+    $("#autoInput2").val("");
+    $("#autoInput3").val("");
+    $("#autoInput4").val("");
+    $("#autoInput5").val("");
+    $("#autoInput6").val("");
+    $("#autoInput7").val("");
+ }
+
+ // 카테고리 X 버튼 클릭
+function detailCloseCategory() {
+    $("#autoInput1").val("");
+    $("#autoInput2").val("");
+    $("#autoInput3").val("");
+    $("#autoInput4").val("");
+    $("#autoInput5").val("");
+    $("#autoInput6").val("");
+    $("#autoInput7").val("");
+}
+
+// 메인 사진 수정 취소 클릭
+function cancelMainPic() {
+
+}
+
+// 메인 사진 수정 X 클릭
+function closeMainPic() {
+
+}
+
+// 특수 문자 제거
+function removeSpecial(str) {
+    var reg = /[\{\}\[\]?,;:|*~`!^\_<>@\#$%&\\\=\'\"]/gi
+    var str = $('input[name=optionValue]').val()
+
+    if(reg.test(str)){
+                console.log($('input[name=optionValue]').val())
+                return $('input[name=optionValue]').attr('value', str.replace(reg, ""));
+            }
+    else {
+                return str;
+            }
+}
+
+// 커스텀 글자 변경
+function changeCustomText(str) {
+    var str1 = $('input[name=text_custom1]').val()
+    var str2 = $('input[name=text_custom2]').val()
+    var str3 = $('input[name=optionValue]').val()
+
+    if(str3.includes(str1)) {
+        return  $('input[name=optionValue]').attr('value', str3.replaceAll(str1, str2));
+    }
+}
+
+// 카테고리 저장
+function insertCategory(str) {
+    var str = $('input[name=category]').attr('value', "생활/건강 > 공구 > 원예 공구 > 농기계");
+
+    return str;
+}
+
+// 배송비 추가
+function changeTax() {
+    var num = $('input[name=beforeTax]').val();
+    var amount = "130,000";
+
+    return $('input[name=beforeTax]').attr('value', amount);
+}
+
+// 옵션명 A - Z 옵션
+function changeNameLiteral() {
+    var opn = $('input[name=optionValue]').val();
+    
+    return  $('input[name=optionValue]').attr('value', "A옵션");
+}
+
+// 옵션명 1 - N 옵션
+function changeNameNumber() {
+    var opn2 = $('input[name=optionValue]').val();
+    
+    return  $('input[name=optionValue]').attr('value', "1옵션");
+}
+
+// 글자 변경
+function changeWord() {
+    var word1 = $('input[name=prefix]').val()
+    var word2 = $('input[name=suffix]').val()
+    var word3 = $('input[name=optionValue]').val()
+
+    if(word3.includes(word1)) {
+        return  $('input[name=optionValue]').attr('value', word3.replaceAll(word1, word2));
+    }
+}
+
+// 글자 변경
+function toChinese() {
+    var ch1 = $('button[name=beforeTrans]').val()
+    var ko2 = $('button[name=afterTrans]').val()
+    var opn3 = $('input[name=optionValue]').val()
+
+    if(opn3.includes(ko2)) {
+        return  $('input[name=optionValue]').attr('value', opn3.replaceAll(ko2, ch1));
+    }
+}
+
+// 글자 변경
+function toKorean() {
+    var ch1 = $('button[name=beforeTrans]').val()
+    var ko2 = $('button[name=afterTrans]').val()
+    var opn3 = $('input[name=optionValue]').val()
+
+    if(opn3.includes(ch1)) {
+        return  $('input[name=optionValue]').attr('value', opn3.replaceAll(ch1, ko2));
+    }
+}
+
+
